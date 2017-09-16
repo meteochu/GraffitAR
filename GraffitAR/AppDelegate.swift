f2267b042a6c5708b4aea412617ea5baac516b34
Andy Liang <meteochu@gmail.com>
2017-09-16 00:48:14 -0400

    Initial Xcode files commit

---
+++ GraffitAR/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  GraffitAR
+//
+//  Created by Andy Liang on 2017-09-16.
+//  Copyright © 2017 GraffitAR. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
